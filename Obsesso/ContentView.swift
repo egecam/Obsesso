e41ee7f12ef5ca76d60587ee28336eff0edf6739
ege <egecam000@gmail.com>
2024-09-18 22:25:10 +0300

    add video player + new ui changes

--- Obsesso/ContentView.swift
+++ Obsesso/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import SwiftData
 import AVFoundation
+import AVKit
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
@@ -19,6 +20,7 @@ struct ContentView: View {
     @StateObject private var camera = CameraModel()
     @State private var showAlert = false
     @State private var trigger = false
+    
     
     var sortedItems: [Item] {
         return items.sorted { $0.timestamp > $1.timestamp}
@@ -36,25 +38,6 @@ struct ContentView: View {
         }
     }
     
-    func calculateTimePhrase(lastTime: Date) -> String {
-        let timeSince = Date().timeIntervalSince(lastTime)
-        let timePhrase: String
-        
-        if timeSince < 1 {
-            timePhrase = "a few seconds"
-        } else if timeSince > 1 && timeSince < 60 {
-            timePhrase = "a minute"
-        } else {
-            if timeSince < 3600 {
-                timePhrase = "hour"
-            } else {
-                timePhrase = "hours"
-            }
-        }
-        
-        return timePhrase
-    }
-    
     var body: some View {
         NavigationSplitView {
             VStack {
@@ -70,19 +53,60 @@ struct ContentView: View {
                 Spacer()
                 
                 if !items.isEmpty {
-                    Text("You have double-checked your \(Text(sortedItems.first!.type).foregroundStyle(.indigo)) \(Text(calculateTimePhrase(lastTime: sortedItems.first!.timestamp)).bold()) ago.")
+                    Text("You have double-checked your \(Text(sortedItems.first!.type).foregroundStyle(.indigo) + Text(" ") + Text(sortedItems.first!.timestamp.timeAgoDisplay()).bold()) ago.")
                         .font(.title2)
                         .padding()
                 } else {
-                    
+                    VStack {
+                        Spacer()
+                        Text("You have not double-checked anything yet.")
+                            .font(.title2)
+                            .padding()
+                        
+                        Button  {
+                            trigger.toggle()
+                            presentSheet = true
+                        }
+                        label: {
+                            Text("Press double-check to start.")
+                                .font(.title2)
+                                .foregroundStyle(.indigo)
+                                .bold()
+                        }
+                        .padding()
+                        .sensoryFeedback(
+                            .impact(weight: .medium, intensity: 0.9),
+                            trigger: trigger
+                        )
+                    }
+                    .multilineTextAlignment(.center)
                 }
                 
                 Spacer()
                 
+                // MARK: ITEMS LIST
                 List {
                     ForEach(sortedItems) { item in
                         NavigationLink {
-                            Text("\(item.title)")
+                            ZStack {
+                                VideoPlayer(player: AVPlayer(url: URL(string: item.videoURL)!))
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                    .ignoresSafeArea()
+                                
+                                HStack {
+                                    VStack {
+                                        Text("\(item.title)")
+                                            .font(.largeTitle.bold())
+                                        
+                                        Text(item.timestamp.timeAgoDisplay())
+                                            .font(.title2)
+                                        
+                                        Spacer()
+                                    }
+                                    Spacer()
+                                }
+                                .padding()
+                            }
                         } label: {
                             Text(item.title).bold() + Text(", ") +
                             Text(item.timestamp, format: Date.FormatStyle(date: .abbreviated, time: .shortened, capitalizationContext: .beginningOfSentence))
@@ -97,24 +121,26 @@ struct ContentView: View {
                     trigger.toggle()
                     presentSheet = true
                 }
-            label: {
+                label: {
                     Text("DOUBLE CHECK")
                         .fontWeight(.bold)
                         .foregroundColor(.white)
                         .frame(width: 200, height: 50)
                         .background(Color.indigo)
-                        .cornerRadius(10)
-            }
-            .padding()
-            .sensoryFeedback(
-                .impact(weight: .medium, intensity: 0.9),
-                trigger: trigger
-            )
+                        .cornerRadius(12)
+                }
+                .padding()
+                .sensoryFeedback(
+                    .impact(weight: .medium, intensity: 0.9),
+                    trigger: trigger
+                )
                 
             }
         } detail: {
             Text("Select an item")
         }
+        
+        // MARK: SHEET
         .sheet(isPresented: $presentSheet, onDismiss: withAnimation(.bouncy) {addItemAction}, content: {
             VStack {
                 Spacer()
@@ -152,77 +178,76 @@ struct ContentView: View {
                         Spacer()
                     }
                     
-                        VStack {
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text("Prove it.")
-                                        .font(.title.bold())
-                                        .padding()
-                                }
-                                
-                                Spacer()
+                    VStack {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("Prove it.")
+                                    .font(.title.bold())
+                                    .padding()
                             }
                             
-                            ZStack {
-                                CameraPreview(camera: camera)
-                                    .frame(width: 400, height: 500)
+                            Spacer()
+                        }
+                        
+                        ZStack {
+                            CameraPreview(camera: camera)
+                                .frame(width: 400, height: 500)
+                            
+                            VStack {
+                                Spacer()
                                 
-                                VStack {
-                                    Spacer()
-                                    
-                                    Button(action: {
-                                        camera.captureVideo()
-                                    }) {
-                                        ZStack {
-                                            Color.gray
-                                                .opacity(0.2)
-                                                .background(.ultraThinMaterial)
-                                                .frame(width: 70, height: 70)
-
-                                            
-                                            Image(systemName: camera.isRecording ? "record.circle.fill" : "record.circle")
-                                                .font(.system(size: 60))
-                                                .foregroundColor(camera.isRecording ? .red : .black)
-                                                
-                                        }
-                                        .roundedCornerWithBorder(lineWidth: 0, borderColor: .gray.opacity(0.2), radius: 50.0, corners: .allCorners)
-                                        .padding(.bottom)
+                                Button(action: {
+                                    camera.captureVideo()
+                                }) {
+                                    ZStack {
+                                        Color.gray
+                                            .opacity(0.2)
+                                            .background(.ultraThinMaterial)
+                                            .frame(width: 70, height: 70)
+                                        
+                                        
+                                        Image(systemName: camera.isRecording ? "record.circle.fill" : "record.circle")
+                                            .font(.system(size: 60))
+                                            .foregroundColor(camera.isRecording ? .red : .black)
+                                        
                                     }
+                                    .roundedCornerWithBorder(lineWidth: 0, borderColor: .gray.opacity(0.2), radius: 50.0, corners: .allCorners)
                                     .padding(.bottom)
                                 }
-                                
-                                if let url = camera.videoURL {
-                                    ZStack {
-                                        Color.clear
-                                            .background(.ultraThinMaterial)
-                                            .clipShape(RoundedRectangle(cornerRadius: 12.0))
-                                            .frame(width: 250, height: 30)
-                                        
-                                        Text("Video recorded: \(url.lastPathComponent)")
-                                            .font(.caption)
-                                            .foregroundColor(.red)
-                                            .background(.ultraThinMaterial)
-                                            .padding()
-                                    }
-                                }
-                                
-                                if let errorMessage = camera.errorMessage {
-                                    ZStack {
-                                        Color.clear
-                                            .background(.ultraThinMaterial)
-                                            .clipShape(RoundedRectangle(cornerRadius: 12.0))
-                                            .frame(width: 250, height: 30)
-                                        
-                                        Text(errorMessage)
-                                            .foregroundColor(.red)
-                                            .background(.ultraThinMaterial)
-                                            .padding()
-                                    }
+                                .padding(.bottom)
+                            }
+                            
+                            if camera.videoURL != nil {
+                                ZStack {
+                                    Color.clear
+                                        .background(.thinMaterial)
+                                        .clipShape(RoundedRectangle(cornerRadius: 12.0))
+                                        .frame(width: 110, height: 25)
+                                    
+                                    Text("Video recorded!")
+                                        .font(.caption.bold())
+                                        .foregroundColor(.indigo)
+                                        .padding()
                                 }
                             }
                             
+                            if let errorMessage = camera.errorMessage {
+                                ZStack {
+                                    Color.clear
+                                        .background(.thinMaterial)
+                                        .clipShape(RoundedRectangle(cornerRadius: 12.0))
+                                        .frame(width: 250, height: Double(errorMessage.count) * 1)
+                                    
+                                    Text(errorMessage)
+                                        .foregroundColor(.red)
+                                        .padding()
+                                }
+                                .frame(width: 250)
+                            }
                         }
-                        .padding()
+                        
+                    }
+                    .padding()
                 }
                 .indexViewStyle(.page(backgroundDisplayMode: .always))
                 .tabViewStyle(.page(indexDisplayMode: .always))
@@ -234,15 +259,15 @@ struct ContentView: View {
                         presentSheet = false
                     }
                 } label: {
-                        Text("Confirm")
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .frame(width: 200, height: 50)
-                            .background(Color.green)
-                            .cornerRadius(10)
-                            .overlay {
-                                title.isEmpty || type.isEmpty || camera.videoURL == nil ? Color.white.opacity(0.4) : nil
-                            }
+                    Text("Confirm")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 50)
+                        .background(Color.green)
+                        .cornerRadius(12)
+                        .overlay {
+                            title.isEmpty || type.isEmpty || camera.videoURL == nil ? Color.white.opacity(0.4).cornerRadius(12) : nil
+                        }
                 }
                 .sensoryFeedback(
                     .impact(weight: .medium, intensity: 0.9),
@@ -258,20 +283,36 @@ struct ContentView: View {
                 showAlert = true
             }
         }
+        .tint(.indigo)
     }
     
+    // MARK: ADD ITEM FUNCTION
     private func addItem(type: String, title: String, videoURL: String) {
         withAnimation {
             let newItem = Item(type: type, title: title, videoURL: videoURL, timestamp: Date())
             modelContext.insert(newItem)
+            
+            do {
+                try modelContext.save()
+            } catch {
+                print("Error saving model context: \(error)")
+            }
+            
             print("New item added: \(newItem)")
         }
     }
     
+    // MARK: ADD ITEM FUNCTION
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             for index in offsets {
                 modelContext.delete(items[index])
+                
+                do {
+                    try modelContext.save()
+                } catch {
+                    print("Error saving model context: \(error)")
+                }
             }
         }
     }
@@ -295,7 +336,34 @@ extension View {
     }
 }
 
+extension Date {
+    func timeAgoDisplay() -> String {
+        
+        let calendar = Calendar.current
+        let minuteAgo = calendar.date(byAdding: .minute, value: -1, to: Date())!
+        let hourAgo = calendar.date(byAdding: .hour, value: -1, to: Date())!
+        let dayAgo = calendar.date(byAdding: .day, value: -1, to: Date())!
+        let weekAgo = calendar.date(byAdding: .day, value: -7, to: Date())!
+        
+        if minuteAgo < self {
+            let diff = Calendar.current.dateComponents([.second], from: self, to: Date()).second ?? 0
+            return "\(diff) sec"
+        } else if hourAgo < self {
+            let diff = Calendar.current.dateComponents([.minute], from: self, to: Date()).minute ?? 0
+            return "\(diff) min"
+        } else if dayAgo < self {
+            let diff = Calendar.current.dateComponents([.hour], from: self, to: Date()).hour ?? 0
+            return "\(diff) hrs"
+        } else if weekAgo < self {
+            let diff = Calendar.current.dateComponents([.day], from: self, to: Date()).day ?? 0
+            return "\(diff) days"
+        }
+        let diff = Calendar.current.dateComponents([.weekOfYear], from: self, to: Date()).weekOfYear ?? 0
+        return "\(diff) weeks"
+    }
+}
+
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+        .modelContainer(for: Item.self)
 }
